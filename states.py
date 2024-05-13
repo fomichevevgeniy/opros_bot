3a4eb036547d53766a5076f55e5a5e287f11e26e
Fomichev Evgeniy <106029893+fomichevevgeniy@users.noreply.github.com>
2024-05-13 19:55:06 +0500

    Initial commit

---
+++ states.py
@@ -0,0 +1,10 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
+class Register(StatesGroup):
+    full_name = State()
+    gender = State()
+    age = State()
+    phone = State()
+    profession = State()
+
